fd06e0f42f320288ff300afcb729ec8c701a34bf
Denis Kolaj <deniskolaj2@gmail.com>
2021-08-11 02:24:00 +0200

    fetch data from the api

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  async function fetchQuotes() {
+    const response = await fetch("http://localhost:5000/api/quotes");
+    const data = await response.json();
+    console.log(data);
+    setQuotes(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {quotes.map((quote) => {
+        return (
+          <div key={quote.id}>
+            <h1>{quote.paragraph}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
