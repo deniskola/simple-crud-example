056d7fa0906e57189a2b4c68d775d5fbd02f086f
Denis Kolaj <deniskolaj2@gmail.com>
2021-08-12 22:21:01 +0200

    add AutoMapper

--- Application/Quotes/Edit.cs
+++ Application/Quotes/Edit.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -16,18 +17,21 @@ namespace Application.Quotes
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
+            private readonly IMapper _mapper;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IMapper mapper)
             {
                 _context = context;
+                _mapper = mapper;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var quote = await _context.Quotes.FindAsync(request.Quote.Id);
 
-                quote.Paragraph = request.Quote.Paragraph ?? quote.Paragraph;
-                quote.Date = request.Quote.Date;
+                _mapper.Map(request.Quote, quote);
+                //quote.Paragraph = request.Quote.Paragraph ?? quote.Paragraph;
+                //quote.Date = request.Quote.Date;
 
                 await _context.SaveChangesAsync();
 
