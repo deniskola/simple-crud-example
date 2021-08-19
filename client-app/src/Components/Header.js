4685f8ef455cc7377bd1cf6b3498e35cbc77cef0
Denis Kolaj <deniskolaj2@gmail.com>
2021-08-19 23:57:50 +0200

    add logo

--- client-app/src/Components/Header.js
+++ client-app/src/Components/Header.js
@@ -1,26 +1,54 @@
 import {Box, Button, Container} from "@material-ui/core";
 import React from "react";
-import {AddCircleOutline} from "@material-ui/icons";
+import {
+  AddCircleOutline,
+  FormatQuoteOutlined,
+  FormatQuote,
+} from "@material-ui/icons";
+import {Grid} from "@material-ui/core";
 
 export default function Header({openForm}) {
   return (
     <Container>
       <Box
-        display="flex"
-        justifyContent="flex-end"
-        style={{backgroundColor: "#00695c", color: "white", marginTop: "20px"}}
+        style={{
+          backgroundColor: "#00695c",
+          color: "white",
+          marginTop: "20px",
+        }}
       >
-        <Button
-          onClick={openForm}
-          variant="outlined"
-          style={{
-            color: "white",
-            border: "0px solid white",
-            margin: "10px 10px 10px 10px",
-          }}
-        >
-          <AddCircleOutline />
-        </Button>
+        <Grid container spacing={3}>
+          <Grid item xs>
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <div id="logo">
+                <FormatQuote
+                  style={{
+                    marginLeft: "10px",
+                    fontSize: "30px",
+                    transform: "rotate(180deg)",
+                  }}
+                />
+                <span>...</span>
+                <FormatQuote style={{fontSize: "30px"}} />
+              </div>
+            </Box>
+          </Grid>
+          <Grid item xs>
+            <Box display="flex" justifyContent="center">
+              <Button
+                onClick={openForm}
+                variant="outlined"
+                style={{
+                  color: "white",
+                  border: "0px solid white",
+                  margin: "10px 10px 10px 10px",
+                }}
+              >
+                <AddCircleOutline />
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
